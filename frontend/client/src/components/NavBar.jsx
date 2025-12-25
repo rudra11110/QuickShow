8831c1422c67f294c301b2a213d301a0e48c2529
rudra11110 <r77182537@gmail.com>
2025-12-25 10:42:33 +0530

    Account Verification from clerk

--- frontend/client/src/components/NavBar.jsx
+++ frontend/client/src/components/NavBar.jsx
@@ -2,17 +2,22 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { MenuIcon, SearchIcon, XIcon } from 'lucide-react'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 function NavBar() {
   const [open, setOpen] = useState(false)
+  const { user } = useUser()
+  const { openSignIn } = useClerk()
 
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-36 py-5'>
-      
+
+      {/* Logo */}
       <Link to='/' className='max-md:flex-1'>
-        <img src={assets.logo} alt="" className='w-36 h-auto' />
+        <img src={assets.logo} alt="logo" className='w-36 h-auto' />
       </Link>
 
+      {/* Menu */}
       <div
         className={`max-md:absolute max-md:top-0 max-md:left-0 z-50 flex flex-col md:flex-row items-center
         max-md:justify-center gap-8 md:px-8 py-3 max-md:h-screen md:rounded-full backdrop-blur
@@ -24,23 +29,34 @@ function NavBar() {
           className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer'
         />
 
-        <Link to='/'>Home</Link>
-        <Link to='/movies'>Movies</Link>
-        <Link to='/'>Theaters</Link>
-        <Link to='/'>Releases</Link>
-        <Link to='/favorite'>Favorites</Link>
+        <Link to='/' onClick={() => setOpen(false)}>Home</Link>
+        <Link to='/movies' onClick={() => setOpen(false)}>Movies</Link>
+        <Link to='/' onClick={() => setOpen(false)}>Theaters</Link>
+        <Link to='/' onClick={() => setOpen(false)}>Releases</Link>
+        <Link to='/favorite' onClick={() => setOpen(false)}>Favorites</Link>
       </div>
 
+      {/* Right section */}
       <div className='flex items-center gap-4'>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer' />
-        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium'>
-          Login
-        </button>
+
+        {!user ? (
+          <button
+            onClick={openSignIn}
+            className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium'
+          >
+            Login
+          </button>
+        ) : (
+          <UserButton />
+        )}
+
         <MenuIcon
           onClick={() => setOpen(true)}
           className='md:hidden w-8 h-8 cursor-pointer'
         />
       </div>
+
     </div>
   )
 }
--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -2,9 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+import { ClerkProvider } from '@clerk/clerk-react'
+
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <ClerkProvider  publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </ClerkProvider>,
 )
